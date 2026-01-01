546bdc86d277e8a8441aa938ca2559ebcd575a52
Abhijit-Jha <abhijeetjha913@gmail.com>
2026-01-01 17:44:41 +0530

    fix: update GITHUB constant to point to the correct repository

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 // DO NOT CHANGE THIS FILE
 export const LINKS = {
   TWITTER: "https://x.com/itshoverr",
-  GITHUB: "https://github.com/itshover/itshover-icons",
+  GITHUB: "https://github.com/itshover/itshover",
   SITE_URL: "https://itshover.com",
   CREATOR: "https://x.com/abhijitwt",
 } as const;
